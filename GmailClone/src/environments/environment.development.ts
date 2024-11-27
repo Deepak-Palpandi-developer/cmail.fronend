d4eedd67c17778124da7c5ad7524b259998159d5
Deepak Palpandi <deepak.palpandi@aravind.org>
2024-11-27 16:13:30 +0530

    Api Url updated

--- GmailClone/src/environments/environment.development.ts
+++ GmailClone/src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   prodution: false,
   hmac_secret_key: 'iY4D5xG5n2OZskDg',
   enable_encryption: true,
-  user_auth_api: 'https://localhost:7273',
+  user_auth_api: 'https://localhost:7195',
   time_zone: 'Asia/Kolkata',
 };
